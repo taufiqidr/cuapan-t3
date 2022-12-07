a71e5cdad54b5581e581798d6824c8c0ae4db9e0
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2022-12-07 10:42:47 +0800

    added basic routing

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <main className="h-full">{children}</main>
+    </>
+  );
+};
+
+export default Layout;
